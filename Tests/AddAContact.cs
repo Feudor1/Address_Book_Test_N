a6e46ed389ad105393035fa5e71c95664bf66b90
Fedor Mishukov <f.mishukov@gmail.com>
2023-06-03 09:26:42 +0300

    Удалили лишнее

--- Tests/AddAContact.cs
+++ Tests/AddAContact.cs
@@ -1,14 +1,3 @@
-using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using OpenQA.Selenium.Firefox;
-using OpenQA.Selenium.Support.UI;
-
 namespace Address_Book_Test_N
 {
     [TestFixture]
