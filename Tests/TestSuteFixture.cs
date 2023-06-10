d436547581a39d0df02b82a37075b97ed900ad2a
f.mishukov <f.mishukov@gmail.com>
2023-06-10 20:15:28 +0700

    Все тесты падают с ошибкой после добавления нового класса (при этом сам класс даже нигде не задействован).

---
+++ Tests/TestSuteFixture.cs
@@ -0,0 +1,22 @@
+﻿namespace Address_Book_Test_N
+{
+    [SetUpFixture]
+    public class TestSuteFixture
+    {
+        public static ApplicationManager app;
+
+        [SetUp]
+        public void InintApplicationManager()
+        {
+            app = new ApplicationManager();
+            app.Navi.GoToHomePage();
+            app.Auth.Login(new AccountData("admin", "secret"));
+        }
+
+        [TearDown]
+        public void StopApplicationManager()
+        {
+            app.Stop();
+        }
+    }
+}
